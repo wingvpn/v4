3afa10d5d1a631ad5a671c39a974332e74346cad
wingvpn <136903398+wingvpn@users.noreply.github.com>
2023-06-17 22:21:05 +0700

    Update slow.sh

--- slow.sh
+++ slow.sh
@@ -2,8 +2,8 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
-echo -e " [INFO] Sabar sayang Lagi Proses"
+echo -e " [INFO] Slow Lagi Proses"
 wget https://raw.githubusercontent.com/Andyvpn/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
-echo -e " [INFO] Selesai Ganti Ns nya Sayang , Enter"
+echo -e " [INFO] Done Ganti Ns nya , Enter"
 sleep 2
 menu
