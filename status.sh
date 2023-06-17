0d9447479f4516340051d28adbf8e4e2b4c106d8
wingvpn <136903398+wingvpn@users.noreply.github.com>
2023-06-17 19:58:39 +0700

    Update status.sh

--- status.sh
+++ status.sh
@@ -132,8 +132,8 @@ clear;clear;clear
 
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo -e "                ${YELLOW}(${NC}${GREEN} STATUS SERVICE INFORMATION ${NC}${YELLOW})${NC}"
-echo -e "         OWNER : ${GREEN} BTR ${NC}${YELLOW}(${NC} ${GREEN} BIGETRO RED ALIEND  ${NC}${YELLOW})${NC}"
-echo -e "       © Copyright By premi vps ${YELLOW}(${NC} 2022-2023 ${YELLOW})${NC}"
+echo -e "         OWNER : ${GREEN} BTR ${NC}${YELLOW}(${NC} ${GREEN} KLMPK.MY.ID  ${NC}${YELLOW})${NC}"
+echo -e "       © Copyright By Andyyuda ${YELLOW}(${NC} 2018-2023 ${YELLOW})${NC}"
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo ""
 echo -e "${RED_BG}                     Sytem Information                    ${NC}"
