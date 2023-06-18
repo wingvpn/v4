a135f65df10249a9939db030cffbb19570ab18ef
wingvpn <136903398+wingvpn@users.noreply.github.com>
2023-06-18 14:13:53 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -292,10 +292,10 @@ echo -e " ${BICyan}└───────────────────�
 
 echo -e "${GREEN}Selama Masih Buatan Manusia Tetap Bisa Di Oprek${NC}" 
 
-echo -e "${GREEN}┌──────────────────────────────────────────────────┐${NC}" 
-echo -e "${GREEN}│  \033[0m ${BOLD}${YELLOW}SSH     VMESS       VLESS      TROJAN $NC" 
-echo -e "${GREEN}│  \033[0m ${Blue} $ssh1        $vma           $vla          $tra $NC" 
-echo -e "${GREEN}└──────────────────────────────────────────────────┘${NC}" 
+echo -e "${GREEN}  ┌──────────────────────────────────────────────────┐${NC}" 
+echo -e "${GREEN}  │  \033[0m ${BOLD}${YELLOW}SSH     VMESS       VLESS      TROJAN $NC" 
+echo -e "${GREEN}  │  \033[0m ${Blue} $ssh1        $vma           $vla          $tra $NC" 
+echo -e "${GREEN}  └──────────────────────────────────────────────────┘${NC}" 
   
 echo -e "     ${BICyan} SSH ${NC}: $ressh"" ${BICyan} NGINX ${NC}: $resngx"" ${BICyan}  XRAY ${NC}: $resv2r"" ${BICyan} TROJAN ${NC}: $resv2r"
 echo -e "   ${BICyan}     STUNNEL ${NC}: $resst" "${BICyan} DROPBEAR ${NC}: $resdbr" "${BICyan} SSH-WS ${NC}: $ressshws"
