95dc0afbce5de4430ead7c72aa6fa363947d3fba
wingvpn <136903398+wingvpn@users.noreply.github.com>
2023-06-26 01:04:31 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -320,10 +320,10 @@ datediff() {
     d2=$(date -d "$2" +%s)
     echo -e " ${BICyan}│$NC  Expiry In     : $(( (d1 - d2) / 86400 )) Days $NC"
 }
-mai="datediff "$Exp" "$DATE"
+mai="datediff "$Exp" "$DATE""
 echo -e " ${BICyan}┌─────────────────────────────────────┐${NC}"
-echo -e " ${BICyan}│  Version       : $(cat /opt/.ver) Last Update ${NC}"
-echo -e " ${BICyan}│  User          :\033[1;36m $Name \e[0m"
+echo -e " ${BICyan}│  Version      ${NC} : v4. Last Update"
+echo -e " ${BICyan}│  User       ${NC}   :\033[1;36m $Name \e[0m"
 if [ $exp \< 1000 ];
 then
 echo -e "   $BICyan│$NC License      : ${GREEN}$sisa_hari$NC Days Tersisa $NC"
