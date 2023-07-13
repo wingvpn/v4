00be0fbdf0ae9c6469c10413e5446bc01a687207
wingvpn <136903398+wingvpn@users.noreply.github.com>
2023-07-13 21:51:46 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -287,8 +287,8 @@ echo -e " ${BICyan}│  ${BICyan}IP-VPS          :  ${BIYellow}$IPVPS${NC}"
 echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
 
 echo -e "${GREEN}  ┌──────────────────────────────────────────────────┐${NC}" 
-echo -e "${GREEN}  │  \033[0m ${BOLD}${YELLOW}SSH     VMESS       VLESS      TROJAN $NC" 
-echo -e "${GREEN}  │  \033[0m ${Blue} $ssh1        $vma           $vla          $tra $NC" 
+echo -e "${GREEN}  │  \033[0m  ${BOLD}${YELLOW}SSH      VMESS      VLESS     TROJAN $NC" 
+echo -e "${GREEN}  │  \033[0m  ${Blue} $ssh1        $vma          $vla         $tra $NC" 
 echo -e "${GREEN}  └──────────────────────────────────────────────────┘${NC}" 
   
 echo -e "     ${BICyan} SSH ${NC}: $ressh"" ${BICyan} NGINX ${NC}: $resngx"" ${BICyan}  XRAY ${NC}: $resv2r"" ${BICyan} TROJAN ${NC}: $resv2r"
